e275ce1b1ee983262539158f9c4eee1573d1327a
Drogebot <Drogebot@hotmail.com>
2018-11-11 22:58:37 +0100

    Asteroids ipv counter

--- gloss-counter/src/Main.hs
+++ gloss-counter/src/Main.hs
@@ -15,7 +15,7 @@ main = do
             highscorefile <- B.readFile "src/highscores.json"
             let highscorefileint = case decode highscorefile of Just (HighscoreEntry _ value) -> value
                                                                 _ -> 0
-            playIO (InWindow "Counter" (768, 512) (0, 0))       -- Or FullScreen
+            playIO (InWindow "Asteroids" (768, 512) (0, 0))       -- Or FullScreen
                 black                                           -- Background color
                 144                                             -- Frames per second
                 initialState{highscore = highscorefileint}      -- Initial state (With the highscore from the highscore file)
